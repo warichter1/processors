8b77c89221908ea5ba0b23c24c540190d404c8aa
William Richter <warichter1@yahoo.com>
2026-01-11 21:30:04 -0500

    Add AI lookup for the table

---
+++ AI/vector.py
@@ -0,0 +1,47 @@
+""" AI vector search"""
+
+from langchain_ollama import OllamaEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+import os
+
+from nvidia import nvidiaLoader, nvidiaHeader, nvidiaSelect
+from arm import armLoader, armSelect # ArmDataProcessor
+from dashConf import dashConfig as conf, dashStyles as style
+from cpu import loadProcessors
+
+def importProcessors(debug=False):
+    processors = loadProcessors(debug=False)
+    amdDf = processors.selectManufacturer('AMD')
+    amdDf[' index'] = range(1, len(amdDf) + 1)
+    intelDf = processors.selectManufacturer('Intel')
+    intelDf[' index'] = range(1, len(intelDf) + 1)
+    otherDf = processors.selectManufacturer('notintelamd')
+    otherDf[' index'] = range(1, len(otherDf) + 1)
+    nvidiaDf = nvidiaLoader('nvidia', columns=nvidiaSelect)
+    nvidiaDf[' index'] = range(1, len(nvidiaDf) + 1)
+    armDf = armLoader('ARM', columns=armSelect)
+    armDf[' index'] = range(1, len(armDf) + 1)
+    df = processors.fullDf
+    return df
+
+embeddings = OllamaEmbeddings(model='mxbai-embed-large')
+dbLocation = "./AI/chromeLangChainDb"
+addDocuments = not os.path.exists(dbLocation)
+vectorStore = Chroma(collection_name="processors", embedding_function=embeddings, persist_directory=dbLocation)
+
+if addDocuments:
+    df = importProcessors(debug=False)
+    documents = []
+    ids = []
+    for i, row in df.iterrows():
+        print(i)
+        document = Document(page_content=row['manufacturer'] + " " + row['processor_family'] + " " + row['microarchitecture'] + " " + str(row['code_name']) + " " + str(row['model']),
+                            metadata={'cores': str(row['hw_ncores']), 'threadspercore': str(row['hw_nthreadspercore']), 'date': str(row['created_at'])},
+                            id=str(i)
+                            )
+        documents.append(document)
+        ids.append(str(i))
+    vectorStore.add_documents(documents=documents, ids=ids)
+retriever = vectorStore.as_retriever(search__kwargs={'k': 5})
--- aiMain.py
+++ aiMain.py
@@ -1,1 +1,29 @@
 """ Working with AI agents """
+
+import pandas as pd
+from datetime import datetime
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+from AI.vector import retriever
+
+
+model = OllamaLLM(model='llama3.2')
+template = """You are an expert in answering questions about processors and Python Dash apps. Answer as concisely as possible.
+               Models: {models}
+               Question: {question}"""
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+while True:
+    print('\n\n----------------------------------------------------------------------------"')
+    question = input('Ask a CPU Question (q to quit): ')
+    print('\n\n----------------------------------------------------------------------------"')
+    if question.lower() == 'q':
+        break
+    startTime = datetime.now()
+    response = retriever.invoke(question)
+    result = chain.invoke({'question': {'question': question}, 'models': ['Nvidia', 'AMD', 'Intel', 'ARM', 'Zilog', 'Motorola']})
+    print(result)
+    endTime = datetime.now()
+    print(f"Lookup Time: {endTime - startTime}")
